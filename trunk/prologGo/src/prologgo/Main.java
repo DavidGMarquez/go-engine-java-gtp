c160b4092b81ed4f5598e4bc5ce318c323ed9e5b
cayette@gmail.com <cayette@gmail.com@d0714ac8-26fc-4a2a-b5cf-49862c343a98>
2011-04-20 19:22:57 +0000

    añadido proyecto prologGo

---
+++ trunk/prologGo/src/prologgo/Main.java
@@ -0,0 +1,54 @@
+package prologgo;
+
+import jpl.*;
+
+/**
+ *
+ * @author Cayette
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String path = "prolog/goRules.pl";
+        Queries q = new Queries();
+        String t1 = "consult('" + path + "')";
+        Query q1 = new Query(t1);
+        System.out.println(t1 + " " + (q1.hasSolution() ? "succeeded" : "failed"));
+
+        q.newGame();
+        if(q.boardsize(9)){
+            System.out.println("Boardsize changed");
+        }else{
+            System.out.println("Illegal boardsize");
+        }
+
+        String t3 = "hasPlayerWithTheMove(0,black)";
+        Query q3 = new Query(t3);
+        System.out.println(t3 + " " + (q3.hasSolution() ? "succeeded" : "failed"));
+        q3.close();
+        Move m = new Move(1, 1, "black");
+
+        if (q.addPiece(m) == false) {
+            System.out.println("Movimiento ilegal");
+        } else {
+            System.out.println(m.toPlayString());
+        }
+        Move m2 = q.genMove("black");
+        System.out.println("genmove B\n= "+m2.toGenString());
+        m2 = q.genMove("white");
+        System.out.println("genmove W\n= "+m2.toGenString());
+        m2 = q.genMove("black");
+        System.out.println("genmove B\n= "+m2.toGenString());
+        m2 = q.genMove("white");
+        System.out.println("genmove W\n= "+m2.toGenString());
+        m2 = q.genMove("black");
+        System.out.println("genmove B\n= "+m2.toGenString());
+        m2 = q.genMove("white");
+        System.out.println("genmove W\n= "+m2.toGenString());
+        m2 = q.genMove("black");
+        System.out.println("genmove B\n= "+m2.toGenString());
+    }
+}
