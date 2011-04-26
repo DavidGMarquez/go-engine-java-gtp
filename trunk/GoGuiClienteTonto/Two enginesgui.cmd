4bd84d2f8a00a018733d7a1c0b03bbc148346f83
Sir.DG.MV@gmail.com <Sir.DG.MV@gmail.com@d0714ac8-26fc-4a2a-b5cf-49862c343a98>
2011-04-26 09:14:38 +0000

    Entrega final

--- trunk/GoGuiClienteTonto/Two enginesgui.cmd
+++ trunk/GoGuiClienteTonto/Two enginesgui.cmd
@@ -1,8 +1,8 @@
-set TONTO="dist/GoGuiCliente.exe"
-set DUMMY="dist/gogui-dummy.exe"
-set GNU="""dist/gnugo.exe --mode gtp"""
+set TONTO="misc/GoGuiCliente.exe"
+set DUMMY="misc/gogui-dummy.exe"
+set GNU="""misc/gnugo.exe --mode gtp"""
 set BLACK=%DUMMY%
 set WHITE=%TONTO%
 set BOARDSIZE=5
-set TWOGTP="gogui-twogtp.exe  -black %BLACK% -white %WHITE% -games 2 -size %BOARDSIZE% -alternate -sgffile pruebadat1"
-gogui.exe -size %BOARDSIZE% -program %TWOGTP% -computer-both -auto 
+set TWOGTP="gogui-twogtp.exe  -black %BLACK% -white %WHITE% -games 2 -size %BOARDSIZE% -alternate -force -xml -sgffile pruebadat1"
+gogui.exe -size %BOARDSIZE% -program %TWOGTP% -computer-both -auto
